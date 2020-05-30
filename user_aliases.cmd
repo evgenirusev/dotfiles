36b15423c46a32357f0be658f4d93325c5292d7b
evgenirusev <vgeni.n.rusev@gmail.com>
2020-05-30 13:47:11 +0300

    update dotfiles

---
+++ user_aliases.cmd
@@ -0,0 +1,32 @@
+;= @echo off
+;= rem Call DOSKEY and use this file as the macrofile
+;= %SystemRoot%\system32\doskey /listsize=1000 /macrofile=%0%
+;= rem In batch mode, jump to the end of the file
+;= goto:eof
+;= Add aliases below here
+e.=explorer .
+gl=git log --oneline --all --graph --decorate  $*
+ls=ls --show-control-chars -F --color $*
+pwd=cd
+clear=cls
+history=cat -n "%CMDER_ROOT%\config\.history"
+unalias=alias /d $1
+vi=vim $*
+cmderr=cd /d "%CMDER_ROOT%"
+
+co=git commit -m $*
+s=git status
+c=git checkout $*
+a=git add .
+r=git reset --hard
+rc=git reset --soft HEAD^
+cl=git clean -fdx
+p=git pull
+po=git pull origin $*
+pu=git push
+cl=git clone $*
+e=start .
+i=npm install
+ds=cd C:/Users/Evgeni/dev/javascript-data-structures-and-algorithms
+nodejs=code C:/Users/Evgeni/dev/NodeJSPractice
+vimrc=vim ~/.vimrc
